69608485d417c495569cb886ecb5026a953b5e98
Guilherme Routar <groutar@gmail.com>
2017-11-15 23:10:39 +0000

    Added example of search request

--- requests/search.js
+++ requests/search.js
@@ -1,6 +1,12 @@
 /**
- * Simple user example request
+ * Repositories search request
+ * In this section we download every repository with a size >= 100000 kb (100 Mb)
+ * 348,019 repositories satisfy the query
  */
+
+ /**
+  * Query the API inbetween ranges and go through pages
+  */
 
 var httpRequest = require('./httpRequest.js');
 
@@ -8,7 +14,7 @@ var userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like
 
 var options = {
   host: 'api.github.com',
-  path: '/users/Guilherme-Routar',
+  path: '/search/repositories?q=size:>100000&page',
   method: 'GET',
   headers: {'user-agent': userAgent} 
 }
