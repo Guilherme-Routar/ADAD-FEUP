f9822b2a727618b60c060beb36738d01e317fb51
Guilherme-Routar <groutar@gmail.com>
2017-12-31 01:53:47 +0000

    Forgot to add commit users to users list -.-

--- requests/addCountries.js
+++ requests/addCountries.js
@@ -7,6 +7,7 @@ var parser = require("csv-to-array");
 var file = require('fs');
 var reposMetadata = ["id", "name", "owner", "full_name", "size", "created_at", "main_language", "n_issues", "n_stargazers", "n_watchers", "n_forks", "n_contributors", "n_commits"];
 var forksMetadata = ["id", "repos_id", "owner", "created_at", "n_forks", "n_stargazers", "n_watchers", "size", "n_issues"];
+var commitsMetadata = ["id", "project_id", "author", "created_at", "additions", "deletions", "total"];
 
 /**
  * Checking if each commit and fork belongs to a repository listed in the file
@@ -22,31 +23,49 @@ parser({
         columns: forksMetadata
     }, function (err, forksArray) {
 
-        var data = [];
-        var inc = 1;
-        var rLen = reposArray.length;
-        var fLen = forksArray.length;
-        var users = [];
+        parser({
+            file: "/home/routar/FEUP/ADAD/ADAD-FEUP/data/commitsData.csv",
+            columns: commitsMetadata
+        }, function (err, commitsArray) {
 
-        for (var i = 0; i < rLen; i++) {
-            var owner = reposArray[i].owner;
-            if (users.indexOf(owner) == -1) {
-                data = [inc, owner, getRandomCountry()];
-                file.appendFileSync("/home/routar/FEUP/ADAD/ADAD-FEUP/data/country.csv", '\n' + data);
-                users.push(owner);
-                inc++;
-            }
-        }
+            var data = [];
+            var inc = 1;
+            var rLen = reposArray.length;
+            var fLen = forksArray.length;
+            var cLen = commitsArray.length;
+            var users = [];
 
-        for (var i = 0; i < fLen; i++) {
-            var owner = forksArray[i].owner;
-            if (users.indexOf(owner) == -1) {
-                data = [inc, owner, getRandomCountry()];
-                file.appendFileSync("/home/routar/FEUP/ADAD/ADAD-FEUP/data/country.csv", '\n' + data);
-                users.push(owner);
-                inc++;
+            for (var i = 0; i < rLen; i++) {
+                var owner = reposArray[i].owner;
+                if (users.indexOf(owner) == -1) {
+                    data = [inc, owner, getRandomCountry()];
+                    file.appendFileSync("/home/routar/FEUP/ADAD/ADAD-FEUP/data/country.csv", '\n' + data);
+                    users.push(owner);
+                    inc++;
+                }
             }
-        }
+
+            for (var i = 0; i < fLen; i++) {
+                var owner = forksArray[i].owner;
+                if (users.indexOf(owner) == -1) {
+                    data = [inc, owner, getRandomCountry()];
+                    file.appendFileSync("/home/routar/FEUP/ADAD/ADAD-FEUP/data/country.csv", '\n' + data);
+                    users.push(owner);
+                    inc++;
+                }
+            }
+
+            for (var i = 0; i < cLen; i++) {
+                var owner = commitsArray[i].author;
+                if (users.indexOf(owner) == -1) {
+                    data = [inc, owner, getRandomCountry()];
+                    file.appendFileSync("/home/routar/FEUP/ADAD/ADAD-FEUP/data/country.csv", '\n' + data);
+                    users.push(owner);
+                    inc++;
+                }
+            }
+
+        });
     });
 });
 
@@ -57,20 +76,20 @@ function getRandomCountry() {
     var countries3 = ['Portugal', 'Spain', 'Italy', 'Belgium'];
 
     var freq = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-                3, 3, 3, 3, 3];
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+        3, 3, 3, 3, 3];
 
     var rand = freq[Math.floor(Math.random() * freq.length)];
 
     var min, max;
 
-    switch(rand) {
+    switch (rand) {
         case 1:
-            return countries1[Math.floor(Math.random()*countries1.length)];
+            return countries1[Math.floor(Math.random() * countries1.length)];
         case 2:
-            return countries2[Math.floor(Math.random()*countries2.length)];
+            return countries2[Math.floor(Math.random() * countries2.length)];
         case 3:
-            return countries3[Math.floor(Math.random()*countries3.length)];
+            return countries3[Math.floor(Math.random() * countries3.length)];
             break;
     }
 }
